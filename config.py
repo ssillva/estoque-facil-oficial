7e06e85379196d3047abab598c6bbac5f6a0dda8
Sandro <sandro2007am@gmail.com>
2023-11-15 10:10:08 -0300

    Update config.py
    Correção da biblioteca de conexão xom o banco de dados

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://usuario:senha@host/bancodedados'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://usuario:senha@host/bancodedados'
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
