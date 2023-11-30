99cf75c3540767ed136cce66d2e3efd419cca7d6
Sandro Silva <sandro2007am@gmail.com>
2023-11-30 17:47:36 -0300

    ajuste no html movimento

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mkradius.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:vertrigo@172.31.255.2/mkradius'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:vertrigo@10.1.29.67/mkradius'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mkradius.db' #para banco local para testar a aplicação
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:vertrigo@172.31.255.2/mkradius' #caso possua o MK-Auth instalado com configurações padrões
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:vertrigo@10.1.29.67/mkradius' #caso tenha colocado o sistema como rede DHCP da sua rede
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
